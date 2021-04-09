0e30676bc1f79f94d3d5b72157bc74b25b2fb2be
Comercial Mix <comercialmixinformatica@gmail.com>
2021-04-09 17:48:29 -0300

    Criando tela inicial

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      
+    <SafeAreaView style={styles.container}>
+      <StatusBar/>
       <View style={styles.header}>
-
+        <View style={styles.sectionHeader}>
+          <TextInput style={styles.input}/>
+          <Button style={styles.button} color={'#0169B2'} title={'Entrar'} />
+          <Button color={'#0169B2'} title={'Cadastre-se'} />
+        </View>
       </View>
-
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 30,
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   header: {
     backgroundColor: '#0169B2',
     width: '100%',
-    height: '8%',
-    
+    justifyContent: 'flex-start',
+  },
+  sectionHeader: {
+    flexDirection: 'row',
+  },
+  input:{
+    backgroundColor: '#fff',
+    width: 200,
+    margin: 10,
+    borderRadius: 5,
   }
 });
